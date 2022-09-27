0cebdc9055e9530144cd277f7a6d05214c6b96f5
Matheus <matheus-rodrigues37@live.com>
2022-09-27 00:34:04 -0300

    config document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from "next"
+import Head from "next/head"
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      Olá Mundo
-    </h1>
+    <>
+      <h1 className="text-5xl">
+        Olá Mundo
+      </h1>
+    </>
   )
 }
 
