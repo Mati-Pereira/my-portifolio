c5520a71ddee840d916a25e9e8391dbebf5b7bf2
Matheus <matheus-rodrigues37@live.com>
2022-09-28 20:10:52 -0300

    skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion"
 
+import { AiFillHtml5 } from 'react-icons/ai'
+
 type Props = {
   directionsLeft?: boolean
 }
@@ -14,7 +16,14 @@ export default function Skill({ directionsLeft }: Props) {
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
+        src='html-5.png'
+        className="rounded-full border border-gray-500 object-cover h-24 w-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out "
       />
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:w-32 xl:h-32 rounded-full z-0 ">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
     </div>
   )
 }
