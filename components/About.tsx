2974bb8d6770e23cd81a1ce0648361d050c4dbac
Matheus <matheus-rodrigues37@live.com>
2022-09-28 17:55:29 -0300

    abou animation

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,30 @@
+import { motion } from "framer-motion"
+
+
 type Props = {}
 
 export default function About({ }: Props) {
   return (
     <div className="flex relative h-screen text-center md:text-left md:flex-row max-w-6xl px-10 justify-evenly mx-auto items-center">
-      <h3>About</h3>
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1
+        }}
+        viewport={{
+          once: true
+        }}
+        transition={{
+          duration: 1
+        }}
+        src="https://github.com/Mati-Pereira.png"
+      />
+
     </div>
   )
 }
--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,27 +1,15 @@
-import { motion } from 'framer-motion'
 import React from 'react'
 
 type Props = {}
 
 export default function BackgroundCircles({ }: Props): JSX.Element {
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        scale: [1, 2, 2, 3, 1],
-        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0]
-      }}
-      transition={{
-        duration: 3,
-      }}
-      className='relative flex justify-center items-center'>
+    <div className='relative flex justify-center items-center animate-pulse'>
       <div className='absolute border border-gray-700 h-[300px] w-[300px] rounded-full mt-64 animate-pulse -z-20' />
-      <div className='absolute border border-[#333] h-[500px] w-[500px] rounded-full mt-64 -z-20' />
+      <div className='absolute border border-[#332e44] h-[500px] w-[500px] rounded-full mt-64 -z-20' />
       <div className='absolute border border-[#4b5f15] h-[650px] w-[650px] rounded-full mt-64 animate-pulse -z-20' />
-      <div className='absolute border border-[#333] h-[800px] w-[800px] rounded-full mt-64 -z-20' />
-    </motion.div>
+      <div className='absolute border border-[#200d0d] h-[800px] w-[800px] rounded-full mt-64 -z-20' />
+    </div>
   )
 }
 
