97d80b52080e4c75da7614b19f17ab250131ebb9
Matheus <matheus-rodrigues37@live.com>
2022-09-29 14:57:39 -0300

    change grid

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,7 +20,7 @@ export default function Skills(): JSX.Element {
       className="h-screen flex relative flex-col textcenter md:text-left xl:dlex-row max-w-[1000px] xl:px-10 min-h-screen justify-center sl:space-y-0 mx-auto items-center px-10 text-center">
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>Hover over a skill for current proficiency</h3>
-      <div className='grid grid-cols-2 md:grid-cols-4 mt-16 gap-5'>
+      <div className='grid grid-cols-3 md:grid-cols-4 mt-16 gap-5'>
         <Skill src='skills/html5.png' percentage='80%' />
         <Skill src='skills/css-3.png' percentage='85%' />
         <Skill src='skills/js.png' percentage='70%' />
