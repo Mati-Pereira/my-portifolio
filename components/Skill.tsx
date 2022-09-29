22554acc608779f711327615d59bc73dacc3394d
Matheus <matheus-rodrigues37@live.com>
2022-09-29 14:53:01 -0300

    finsh for now

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion"
 
 type Props = {
-  directionsLeft?: boolean
+  src: string,
+  percentage: string
 }
 
-export default function Skill({ directionsLeft }: Props) {
+export default function Skill({ src, percentage }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
@@ -15,12 +16,12 @@ export default function Skill({ directionsLeft }: Props) {
         animate={{ x: 0 }}
         transition={{ duration: 0.5 }}
         whileInView={{ opacity: 1 }}
-        src="html-5.png"
+        src={src}
         className="rounded-3xl border border-gray-500 object-cover h-24 w-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out "
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:w-32 xl:h-32 rounded-3xl z-0 ">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className="text-3xl font-bold text-black opacity-100">{percentage}</p>
         </div>
       </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,31 +4,33 @@ import Skill from './Skill'
 export default function Skills(): JSX.Element {
   return (
     <motion.div
-    initial={{
-      opacity: 0,
-      y: 20,
-    }}
-    animate={{
-      opacity: 1,
-      y: 0,
-    }}
-    transition={{
-      ease: 'easeOut',
-      duration: 1.5,
-    }}
+      initial={{
+        opacity: 0,
+        y: 20,
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{
+        ease: 'easeOut',
+        duration: 1.5,
+      }}
 
       className="h-screen flex relative flex-col textcenter md:text-left xl:dlex-row max-w-[1000px] xl:px-10 min-h-screen justify-center sl:space-y-0 mx-auto items-center px-10 text-center">
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>Hover over a skill for current proficiency</h3>
       <div className='grid grid-cols-2 md:grid-cols-4 mt-16 gap-5'>
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill src='skills/html5.png' percentage='80%' />
+        <Skill src='skills/css-3.png' percentage='85%' />
+        <Skill src='skills/js.png' percentage='70%' />
+        <Skill src='skills/nodejs.png' percentage='60%' />
+        <Skill src='skills/react.png' percentage='70%' />
+        <Skill src='skills/typescript.png' percentage='60%' />
+        <Skill src='skills/git.png' percentage='60%' />
+        <Skill src='skills/nextjs.png' percentage='60%' />
+        <Skill src='skills/nestjs.png' percentage='20%' />
+        <Skill src='skills/github.png' percentage='50%' />
       </div>
     </motion.div>
   )
