fe8a5efc2118eada470ea24b4876418e281303cf
Matheus <matheus-rodrigues37@live.com>
2022-11-21 12:11:56 -0300

    up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="shortcut icon" href="/static/favicon.png" />
+                    <link rel="shortcut icon" href="/favicon.png" />
                 </Head>
                 <body>
                     <Main />
