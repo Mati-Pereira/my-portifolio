d45d6067428426a3ba16362092dfac39adc749cc
Matheus <matheus-rodrigues37@live.com>
2023-01-26 15:50:38 -0300

    up

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,24 +1,29 @@
-import { init, sendForm } from 'emailjs-com';
-init('PL6RvQ2mJ6MgZ7ycK');
+import { init, sendForm } from "emailjs-com";
+init("PL6RvQ2mJ6MgZ7ycK");
 
 import { SubmitHandler, useForm } from "react-hook-form";
 
-import { toast, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from "react-toastify";
 
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 type Inputs = {
-  name: string,
-  email: string,
-  subject: string,
-  message: string
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 };
 
 export default function ContactMe() {
   const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = ({ email, message, name, subject }) => {
+  const onSubmit: SubmitHandler<Inputs> = ({
+    email,
+    message,
+    name,
+    subject,
+  }) => {
     if (!email || !message || !name || !subject) {
-      toast.error('Missing required field, please fill them', {
+      toast.error("Alguns campos estão faltando, por favor preencha-os", {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -29,9 +34,9 @@ export default function ContactMe() {
       });
       return;
     }
-    sendForm('default_service', 'template_irvv74i', '#contact-form')
-      .then(function (response) {
-        toast.success('Form send successfully', {
+    sendForm("default_service", "template_irvv74i", "#contact-form").then(
+      function (response) {
+        toast.success("Email mandado com sucesso", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -40,10 +45,10 @@ export default function ContactMe() {
           draggable: true,
           progress: undefined,
         });
-        console.log('SUCCESS!', response.status, response.text);
-
-      }, function (error) {
-        toast.error('Error sending the form', {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        toast.error("Erro ao mandar o email", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -52,30 +57,64 @@ export default function ContactMe() {
           draggable: true,
           progress: undefined,
         });
-        console.log('FAILED...', error);
-      });
+        console.log("FAILED...", error);
+      }
+    );
   };
   return (
-    <div className="h-screen flex relative flex-col text-center md:text-left  max-w-6xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Contact</h3>
+    <div className="relative flex flex-col items-center h-screen max-w-6xl px-10 mx-auto text-center md:text-left justify-evenly">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contatos
+      </h3>
       <div className="flex flex-col mt-24 md:mt-32">
-        <h4 className="text-3xl md:text-4xl font-semibold text-center">Interested, get in contact and <span className="decoration-yellow-600/50 underline">Let&apos;s Talk</span></h4>
+        <h4 className="text-3xl font-semibold text-center md:text-4xl">
+          Interessado?, entre em contato e
+          <span className="underline decoration-yellow-600/50">
+            Vamos Conversar
+          </span>
+        </h4>
       </div>
 
-      <div className='space-y-6 md:space-y-10'>
-
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-full md:w-fit mx-auto' id='contact-form'>
-
-          <div className='flex flex-col md:flex-row md:space-x-2 gap-2 md:gap-0 w-full'>
-            <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
-            <input {...register('email')} placeholder='Email' className='contactInput' type="email" />
+      <div className="space-y-6 md:space-y-10">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col w-full mx-auto space-y-2 md:w-fit"
+          id="contact-form"
+        >
+          <div className="flex flex-col w-full gap-2 md:flex-row md:space-x-2 md:gap-0">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
           </div>
 
-          <input {...register('subject')} placeholder='Subject' className='contactInput' type="text" />
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
 
-          <textarea {...register('message')} placeholder='Message' className='contactInput' />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
 
-          <button type='submit' className='bg-yellow-600 py-5 px-10 rounded-md text-black font-bold hover:bg-yellow-600/40 hover:text-white duration-200'>Submit</button>
+          <button
+            type="submit"
+            className="px-10 py-5 font-bold text-black duration-200 bg-yellow-600 rounded-md hover:bg-yellow-600/40 hover:text-white"
+          >
+            Submit
+          </button>
           <ToastContainer
             position="top-right"
             autoClose={5000}
@@ -90,5 +129,5 @@ export default function ContactMe() {
         </form>
       </div>
     </div>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
-import { motion } from 'framer-motion';
-import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion";
+import { SocialIcon } from "react-social-icons";
 
 function Header(): JSX.Element {
   return (
-    <header className='sticky top-0 flex items-start justify-between max-w-6xl mx-auto z-20 xl:items-center p-5'>
+    <header className="sticky top-0 z-20 flex items-start justify-between max-w-6xl p-5 mx-auto xl:items-center">
       <motion.div
         initial={{
           x: -500,
@@ -13,15 +13,29 @@ function Header(): JSX.Element {
         animate={{
           x: 0,
           opacity: 1,
-          scale: 1
+          scale: 1,
         }}
         transition={{
           duration: 1,
-          ease: 'easeOut',
+          ease: "easeOut",
         }}
       >
-        <SocialIcon className='hover:animate-pulse' target="_blank" network='github' url='https://github.com/Mati-Pereira' bgColor='transparent' fgColor='gray' />
-        <SocialIcon className='hover:animate-pulse' target="_blank" network='linkedin' url='https://www.linkedin.com/in/matheus-rodrigues-pereira/' bgColor='transparent' fgColor='gray' />
+        <SocialIcon
+          className="hover:animate-pulse"
+          target="_blank"
+          network="github"
+          url="https://github.com/Mati-Pereira"
+          bgColor="transparent"
+          fgColor="gray"
+        />
+        <SocialIcon
+          className="hover:animate-pulse"
+          target="_blank"
+          network="linkedin"
+          url="https://www.linkedin.com/in/matheus-rodrigues-pereira/"
+          bgColor="transparent"
+          fgColor="gray"
+        />
       </motion.div>
       <motion.div
         initial={{
@@ -32,20 +46,28 @@ function Header(): JSX.Element {
         animate={{
           x: 0,
           opacity: 1,
-          scale: 1
+          scale: 1,
         }}
         transition={{
           duration: 1,
-          ease: 'easeOut',
+          ease: "easeOut",
         }}
-      className='flex flex-row items-center text-gray-300 cursor-pointer'>
-        <SocialIcon className='hover:animate-pulse' target='_blank' network='email' url='mailto:matheus-rodrigues37@live.com' bgColor='transparent' fgColor='gray' />
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="hover:animate-pulse"
+          target="_blank"
+          network="email"
+          url="mailto:matheus-rodrigues37@live.com"
+          bgColor="transparent"
+          fgColor="gray"
+        />
+        <p className="hidden text-sm text-gray-400 uppercase md:inline-flex">
+          Entre em contato
+        </p>
       </motion.div>
     </header>
   );
 }
 
 export default Header;
-
-
