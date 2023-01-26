6770be32d4ba95e34fda6335e5297a4391af1786
Matheus <matheus-rodrigues37@live.com>
2023-01-26 15:47:45 -0300

    up

--- components/About.tsx
+++ components/About.tsx
@@ -12,10 +12,10 @@ export default function About() {
       transition={{
         duration: 1,
       }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-6xl px-10 justify-evenly mx-auto items-center"
+      className="relative flex flex-col items-center h-screen max-w-6xl px-10 mx-auto text-center md:text-left md:flex-row justify-evenly"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        About
+        Sobre
       </h3>
       <motion.img
         initial={{
@@ -35,23 +35,25 @@ export default function About() {
         src="https://github.com/Mati-Pereira.png"
         className="md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-xl xl:w-[300px] xl:h-[400px] mt-24"
       />
-      <div className="space-y-5 px-0 md:px-10 lg:px-28">
+      <div className="px-0 space-y-5 md:px-10 lg:px-28">
         <h4 className="text-4xl font-semibold lg:mb-14">
-          Here is a little of{" "}
-          <span className="underline decoration-yellow-700 ">My Story</span>
+          Aqui está um pouco da minha história
+          <span className="underline decoration-yellow-700 ">
+            Minha História
+          </span>
         </h4>
-        <p className="text-xs sm:text-base text-justify ">
-          Hello, how are you? My name is Matheus Rodrigues Pereira and I thank
-          you for taking the time to look at this portfolio. I am 25 years old
-          and currently unemployed. I graduated in Electrical Engineering from
-          Universidade Vale do Rio Doce, in the year 2019.
+        <p className="text-xs text-justify sm:text-base ">
+          Olá, como vai? Meu nome é Matheus Rodrigues Pereira e agradeço por
+          dedicar seu tempo para olhar este portfólio. Tenho 25 anos e
+          atualmente estou desempregado. Sou formado em Engenharia Elétrica pela
+          Universidade Vale do Rio Doce, no ano de 2019.
         </p>
-        <p className="text-xs sm:text-base text-justify ">
-          Programming has always been a passion of mine, since college, I always
-          thought of connecting my knowledge with new technologies, it was and
-          is something very instinctive to me. I dedicate myself heart and soul
-          to achieve my dream of being a{" "}
-          <span className="underline decoration-yellow-700 font-bold">
+        <p className="text-xs text-justify sm:text-base ">
+          Programação sempre foi uma paixão minha, desde a faculdade, sempre
+          pensei em conectar meus conhecimentos com novas tecnologias, foi e é
+          algo muito instintivo para mim. Dedico-me de corpo e alma para
+          realizar o meu sonho de ser um
+          <span className="font-bold underline decoration-yellow-700">
             Front-end Web Developer.
           </span>
         </p>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,39 +1,49 @@
-import Link from "next/link"
-import { Cursor, useTypewriter } from "react-simple-typewriter"
-import BackgroundCircles from "./BackgroundCircles"
+import Link from "next/link";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
 
 export default function Hero(): JSX.Element {
   const [text, count] = useTypewriter({
-    words:
-      [
-        "Hi, My Name is Matheus",
-        "I'm a Front-end Developer",
-        "Nice to meet you",
-        "Fun Fact: 'I Love Dogs!'",
-        "Coffee is the best",
-        "#neverstoplearning"
-      ],
+    words: [
+      "Hi, My Name is Matheus",
+      "I'm a Front-end Developer",
+      "Nice to meet you",
+      "Fun Fact: 'I Love Dogs!'",
+      "Coffee is the best",
+      "#neverstoplearning",
+    ],
     loop: true,
     delaySpeed: 2000,
-  })
+  });
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-y-hidden">
+    <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-y-hidden text-center">
       <BackgroundCircles />
-      <img className="rounded-full h-32 w-32 mx-auto" src="https://github.com/Mati-Pereira.png" alt="profile-picture" />
+      <img
+        className="w-32 h-32 mx-auto rounded-full"
+        src="https://github.com/Mati-Pereira.png"
+        alt="profile-picture"
+      />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[12px]">Fullstack Web Developer</h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[12px]">
+          Front-end Web Developer
+        </h2>
+        <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
           <span className="mr-3 ">{text}</span>
           <Cursor cursorColor=" #5E8874" />
         </h1>
         <div className="pt-5">
-          <Link href="#about"><button className="heroButton">About</button></Link>
-          <Link href="#skills"><button className="heroButton">Skills</button></Link>
-          <Link href="#projects"><button className="heroButton">Projects</button></Link>
+          <Link href="#about">
+            <button className="heroButton">Sobre</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Habilidades</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projetos</button>
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
